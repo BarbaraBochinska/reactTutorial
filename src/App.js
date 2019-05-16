b947481377b77fd97ae4ed4a9da3ddf9fe08bfc8
Szymon Nowakm <sznowak13@gmail.com>
2019-05-16 20:35:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from "./Table";
+import Header from "./Header";
+// import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    vehicles: []
+  };
+
+  componentDidMount() {
+    const url = "https://swapi.co/api/vehicles";
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          vehicles: result.results
+        });
+      });
+  }
+
+  removeVehicle = index => {
+    const { vehicles } = this.state;
+
+    this.setState({
+      vehicles: vehicles.filter((vehicle, i) => {
+        return i !== index;
+      })
+    });
+  };
+
+  // handleSubmit = vehicle => {
+  //   this.setState({ vehicles: [...this.state.vehicles, vehicle] });
+  // };
+
+  render() {
+    const { vehicles } = this.state;
+
+    return (
+      <div className="full-container">
+        <Header />
+        <Table vehicleData={vehicles} removeVehicle={this.removeVehicle} />
+        {/* <Form handleSubmit={this.handleSubmit} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  render() {
+    const { vehicleData, removeVehicle } = this.props;
+
+    return (
+      <table>
+        <TableHeader />
+        <TableBody vehicleData={vehicleData} removeVehicle={removeVehicle} />
+      </table>
+    );
+  }
+}
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Model</th>
+        <th>Cost in Credits</th>
+        <th>Crew</th>
+        <th>Passengers</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = props => {
+  const rows = props.vehicleData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.model}</td>
+        <td>{row.cost_in_credits}</td>
+        <td>{row.crew}</td>
+        <td>{row.passengers}</td>
+        <td>
+          <button onClick={() => props.removeVehicle(index)}>Delete</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+
+export default Table;
